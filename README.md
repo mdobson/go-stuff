d03ee5ed74f4e4d47d544c8f01b10a962b8e643f
Matt Dobson <mdobson4@gmail.com>
2013-04-28 10:04:49 -0400

    Fixed channel example to bit a tad more compelling.

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Simple examples of programs written in Go.
 
 2. Showing concurrent execution with goroutines
 
-3. Send message on channel and when we have one print it out and exit the program
+3. Send a message between two goroutines using a channel and the ```select``` statement
+
+4. Simple reverse proxy using the standard lib.
 
 [Really good resource of explained Go examples. Shamelessly stealing code from this for my own sake.](https://gobyexample.com/)
 
--- chan.go
+++ chan.go
@@ -3,10 +3,32 @@ package main
 import "fmt"
 
 func main() {
+
+	//Method of communication between them
 	messages := make(chan string)
 
-	go func(){ messages <- "ping"}()
+	//Separate process
+	//We Queue up a simple ping message into the channel.
+	go func(){ 
+		fmt.Println("Queue message into channel in goroutine")
+		messages <- "ping"
+	}()
 
-	msg:= <-messages
-	fmt.Println(msg)	
+
+	//Separate process
+	go func(){
+		//We use a loop to keep the process alive
+		//The select statement to waits for the channel to send a message to us.
+		for {
+			select {
+				case msg:= <-messages:
+					fmt.Println("Dequeue message from channel:",msg)
+			}
+		}
+	}()
+
+	
+	fmt.Println("End of main execution wait for input...")
+	var input string
+    fmt.Scanln(&input)	
 }
