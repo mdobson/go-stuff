face960f970ce479cca90b98f91480e8e9a7e49f
Matt Dobson <mdobson4@gmail.com>
2013-04-29 09:38:54 -0400

    Fill out the reverse proxy sample.

--- proxy.go
+++ proxy.go
@@ -2,12 +2,17 @@ package main
 
 import "fmt"
 import "net/http"
+import "net/url"
 import "net/http/httputil"
 
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	println(fmt.Sprintf("REQUEST:%s", r.URL.Path))
-	proxy := httputil.NewSingleHostReverseProxy(r.URL)
+	url, err := url.Parse("http://127.0.0.1:1337/")
+	if err != nil {
+		panic(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(url)
 	proxy.ServeHTTP(w, r)
 }
 
